fea970774804318aac459d7b52745d79260a8765
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-02-21 15:23:42 +0300

    create todoListTopNavigatorHeader

---
+++ src/components/TodoListTopNavigatorHeader/index.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from 'react';
+import {View, TouchableOpacity} from 'react-native';
+import {style} from './style';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import {COLORS} from '../../COLORS';
+import {useNavigation} from '@react-navigation/native';
+import {TodoListTopNavigationProp} from '../../navigators/TodoListTopNavigator/type';
+import {nameScreen} from '../../navigators/nameScreen';
+
+export const TodoListTopNavigatorHeader: FC = () => {
+  const navigation = useNavigation<TodoListTopNavigationProp>();
+
+  const navigateAddNewItemScreen = () => {
+    navigation.navigate(nameScreen.ADD_NEW_ITEM_SCREEN);
+  };
+
+  return (
+    <View style={style.header}>
+      <TouchableOpacity onPress={navigateAddNewItemScreen}>
+        <FontAwesomeIcon icon={faPlus} size={25} color={COLORS.white} />
+      </TouchableOpacity>
+    </View>
+  );
+};
