0c5101dd3948deb9236821a6b0211df4ff6d4c5e
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-02-21 14:42:49 +0300

    create sign in button

---
+++ src/components/SignInBotton/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import {style} from './style';
+import {StatusBar, Text, TouchableOpacity, View} from 'react-native';
+
+export const SignInBotton = () => {
+  const googleSignIn = async () => {
+    const {idToken} = await GoogleSignin.signIn();
+    console.log(idToken);
+
+    // Create a Google credential with the token
+    const googleCredential = await auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    const res = await auth().signInWithCredential(googleCredential);
+    console.log(res);
+  };
+
+  const googleSignOut = async () => {
+    auth()
+      .signOut()
+      .then(() => {
+        console.log('user out');
+      });
+  };
+
+  return (
+    <View style={style.container}>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
+      <TouchableOpacity style={style.btn} onPress={googleSignIn}>
+        <Text style={style.font}>{'Google Sign In'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={style.btn} onPress={googleSignOut}>
+        <Text style={style.font}>{'Google Sign out'}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
---
+++ src/components/SignInBotton/style.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btn: {
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: 'blue',
+  },
+  font: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: 'blue',
+  },
+});
