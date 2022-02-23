a6394fbc8912d72cc2ddaf8e61da42b74ca56100
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-02-23 11:14:07 +0300

    delete comment

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,6 @@ import { createStore } from 'redux';
 import { todoReducer, TodoReducerState } from './reducers/TodoReducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// defaults to localStorage for web
 
 const persistConfig = {
   key: 'root',
