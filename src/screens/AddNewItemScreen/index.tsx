eb42b10368cf56fb4f2a948d7eeb2f2e0acb82f0
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-02-27 16:26:35 +0300

    set item for user

--- src/screens/AddNewItemScreen/index.tsx
+++ src/screens/AddNewItemScreen/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, FC } from 'react';
 import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { style } from './style';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addItemAction } from '../../redux/actions/todoActions/addItemAction';
 import { COLORS } from '../../COLORS';
 import { createNewItemHelper } from '../../helpers/createNewItemHelper';
@@ -13,23 +13,26 @@ import { firebase } from '@react-native-firebase/database';
 export const AddNewItemScreen: FC = () => {
   const navigation = useNavigation<AddNewItemScreenNavigationProps>();
   const dispatch = useDispatch();
+  const userToken = useSelector(state => {
+    return state.userToken;
+  });
 
   const [text, setText] = useState<string>('');
   const buttonStyle = text ? style.button : style.buttonDis;
 
-  const setData = async newItem => {
+  const setData = async (newItem, userToken) => {
     await firebase
       .app()
       .database(
-        'https://fir-2f0d3-default-rtdb.europe-west1.firebasedatabase.app/',
+        `https://fir-2f0d3-default-rtdb.europe-west1.firebasedatabase.app/`,
       )
-      .ref('/Todo')
+      .ref(`Users/User${userToken}/Todo`)
       .push(newItem);
   };
 
-  const addItem = (text: TodoItemType['text']) => {
+  const addItem = async (text: TodoItemType['text']) => {
     const newItem = createNewItemHelper(text);
-    setData(newItem);
+    await setData(newItem, userToken);
     dispatch(addItemAction({ newItem }));
   };
 
