be9d665ec12618c769a64680dfb27e6706796f96
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-03-03 12:37:11 +0300

    create signInSaga

---
+++ src/redux/saga/SignInSaga.ts
@@ -0,0 +1,23 @@
+import { put } from 'redux-saga/effects';
+import { signInAction } from '../actions/todoActions/signInAction';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import { createReferenceHelper } from '../../helpers/createReferenceHelper';
+
+export function* helloSaga() {
+  const { idToken } = yield GoogleSignin.signIn();
+  console.log('IDTOKEN', idToken);
+  // Create a Google credential with the token
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  // Sign-in the user with the credential
+  const { user } = yield auth().signInWithCredential(googleCredential);
+  console.log('USER', user);
+
+  const userToken = user.uid;
+  yield createReferenceHelper
+    .ref(`/Users/`)
+    .child(`${userToken}`)
+    .set(userToken);
+
+  yield put(signInAction({ userToken, user }));
+}
