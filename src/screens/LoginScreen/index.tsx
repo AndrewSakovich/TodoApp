ed65ce46d00d9b483ca5e46c3b43c89a856f690d
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-03-03 12:38:59 +0300

    add signInSaga

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,55 +1,14 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import { View } from 'react-native';
 import { style } from './style';
 import { SignInButton } from '../../components/SignInButton';
 import { useDispatch } from 'react-redux';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
-import {
-  signInAction,
-  SignInPayload,
-} from '../../redux/actions/todoActions/signInAction';
-import { firebase } from '@react-native-firebase/database';
-import { createReferenceHelper } from '../../helpers/createReferenceHelper';
+import { TodoActionTypes } from '../../redux/actions/todoActions';
 
 export const LoginScreen: FC = () => {
-  const [users, setUsers] = useState<SignInPayload['userToken'][]>([]);
-
-  useEffect(() => {
-    const userArr = createReferenceHelper.ref('Users').on('value', snapshot => {
-      const obj = snapshot.val() ?? {};
-      const arr: SignInPayload['userToken'][] = Object.keys(obj);
-      setUsers(arr);
-    });
-
-    return () => createReferenceHelper.ref('Users').off('child_added', userArr);
-  }, []);
-
-  const setData = async (userToken: SignInPayload['userToken']) => {
-    const check = users.includes(userToken);
-
-    if (!check) {
-      return await createReferenceHelper
-        .ref(`/Users/`)
-        .child(`${userToken}`)
-        .set(userToken);
-    }
-  };
-
   const dispatch = useDispatch();
 
-  const googleSignIn = async () => {
-    const { idToken } = await GoogleSignin.signIn();
-
-    // Create a Google credential with the token
-    const googleCredential = await auth.GoogleAuthProvider.credential(idToken);
-    // Sign-in the user with the credential
-    const { user } = await auth().signInWithCredential(googleCredential);
-    const userToken = user.uid;
-    await setData(userToken);
-
-    dispatch(signInAction({ userToken, user }));
-  };
+  const googleSignIn = dispatch({ type: TodoActionTypes.SIGN_IN_SAGA });
 
   return (
     <View style={style.container}>
