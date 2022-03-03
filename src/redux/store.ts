d36af69df1444d8496d3d66effc52f9af66b1164
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-03-03 12:36:45 +0300

    connect saga with redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { todoReducer, TodoReducerState } from './reducers/TodoReducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import createSagaMiddleware from 'redux-saga';
+import { helloSaga } from './saga/SignInSaga';
+import { takeEvery } from 'redux-saga/effects';
+import { SignInAction } from './actions/todoActions/signInAction';
+import { TodoActionTypes } from './actions/todoActions';
 
 const persistConfig = {
   key: 'root',
@@ -9,7 +14,17 @@ const persistConfig = {
 };
 
 export type ReduxStoreType = TodoReducerState;
+const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, todoReducer);
-export const store = createStore(persistedReducer);
+export const store = createStore(
+  persistedReducer,
+  applyMiddleware(sagaMiddleware),
+);
 export const persistor = persistStore(store);
+
+sagaMiddleware.run(rootWatcher);
+
+function* rootWatcher() {
+  yield takeEvery(TodoActionTypes.SIGN_IN_SAGA, helloSaga);
+}
