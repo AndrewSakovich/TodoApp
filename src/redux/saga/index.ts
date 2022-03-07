f5313a1f56144ef7fe1998fa105ebe3b43144c17
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-03-07 15:12:59 +0300

    remove rootWatcher in index.ts

---
+++ src/redux/saga/index.ts
@@ -0,0 +1,16 @@
+import { takeEvery } from 'redux-saga/effects';
+import { AuthSagaActions } from '../actions/authSagaActions';
+import { signInSaga } from './SignInSaga';
+import { signOutSaga } from './SignOutSaga';
+import { TodoSagaActions } from '../actions/todoSagaActions';
+import { addItemSaga } from './adItemSaga';
+import { doneItemSaga } from './doneItemSaga';
+import { deleteItemSaga } from './deleteItemSaga';
+
+export function* rootWatcher() {
+  yield takeEvery(AuthSagaActions.SIGN_IN_SAGA, signInSaga);
+  yield takeEvery(AuthSagaActions.SIGN_OUT_SAGA, signOutSaga);
+  yield takeEvery(TodoSagaActions.ADD_ITEM_SAGA, addItemSaga);
+  yield takeEvery(TodoSagaActions.DONE_ITEM_SAGA, doneItemSaga);
+  yield takeEvery(TodoSagaActions.DELETE_ITEM_SAGA, deleteItemSaga);
+}
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { todoReducer, TodoReducerState } from './reducers/TodoReducer';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistReducer, persistStore } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import createSagaMiddleware from 'redux-saga';
-import { signInSaga } from './saga/SignInSaga';
-import { takeEvery } from 'redux-saga/effects';
-import { AuthSagaActions } from './actions/authSagaActions';
-import { signOutSaga } from './saga/SignOutSaga';
-import { TodoSagaActions } from './actions/todoSagaActions';
-import { addItemSaga } from './saga/adItemSaga';
-import { doneItemSaga } from './saga/doneItemSaga';
-import { deleteItemSaga } from './saga/deleteItemSaga';
+import { rootWatcher } from './saga';
 
 const persistConfig = {
   key: 'root',
@@ -28,11 +21,3 @@ export const store = createStore(
 export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootWatcher);
-
-function* rootWatcher() {
-  yield takeEvery(AuthSagaActions.SIGN_IN_SAGA, signInSaga);
-  yield takeEvery(AuthSagaActions.SIGN_OUT_SAGA, signOutSaga);
-  yield takeEvery(TodoSagaActions.ADD_ITEM_SAGA, addItemSaga);
-  yield takeEvery(TodoSagaActions.DONE_ITEM_SAGA, doneItemSaga);
-  yield takeEvery(TodoSagaActions.DELETE_ITEM_SAGA, deleteItemSaga);
-}
