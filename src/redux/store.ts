4f39284a3dc2a08a55bbcea384f9a219c6a90bff
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-03-09 11:42:16 +0300

    refactor imports

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,14 +3,6 @@ import { todoReducer, TodoReducerState } from './reducers/TodoReducer';
 import { persistReducer, persistStore } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import createSagaMiddleware from 'redux-saga';
-import { takeEvery } from 'redux-saga/effects';
-import { AuthSagaActions } from './actions/authSagaActions';
-import { signInSaga } from './saga/signInSaga';
-import { signOutSaga } from './saga/signOutSaga';
-import { TodoSagaActions } from './actions/todoSagaActions';
-import { addItemSaga } from './saga/adItemSaga';
-import { doneItemSaga } from './saga/doneItemSaga';
-import { deleteItemSaga } from './saga/deleteItemSaga';
 import { rootWatcher } from './saga';
 
 const persistConfig = {
