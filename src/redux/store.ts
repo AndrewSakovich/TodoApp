8f50a7007b4441744a9a441390786c132694c988
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-03-10 11:02:18 +0300

    refactor store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
-import { todoReducer, TodoReducerState } from './reducers/TodoReducer';
-import { persistReducer, persistStore } from 'redux-persist';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { TodoReducerState } from './reducers/TodoReducer';
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
 import { rootWatcher } from './saga';
 import { rootReducer } from './reducers/rootReducer';
 import { AuthReducerState } from './reducers/authReducer';
-
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-};
 
 export type ReduxStoreType = {
   todo: TodoReducerState;
@@ -18,11 +12,7 @@ export type ReduxStoreType = {
 };
 const sagaMiddleware = createSagaMiddleware();
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-export const store = createStore(
-  persistedReducer,
-  applyMiddleware(sagaMiddleware),
-);
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootWatcher);
