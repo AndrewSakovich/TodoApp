9d6b18ad1f34b90410676e8983d23ce890ccb37b
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-03-10 11:01:31 +0300

    set in persist auth

--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -2,8 +2,15 @@ import { combineReducers } from 'redux';
 import {} from './TodoReducer';
 import { todoReducer } from './TodoReducer';
 import { authReducer } from './authReducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistReducer } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
 export const rootReducer = combineReducers({
   todo: todoReducer,
-  auth: authReducer,
+  auth: persistReducer(persistConfig, authReducer),
 });
