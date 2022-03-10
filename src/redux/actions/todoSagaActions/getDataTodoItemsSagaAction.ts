cbd12e477e586de1dbe09a08f7de30eb083b835d
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-03-10 11:00:42 +0300

    refactor getDataTodoItemsSagaAction.ts

--- src/redux/actions/todoSagaActions/getDataTodoItemsSagaAction.ts
+++ src/redux/actions/todoSagaActions/getDataTodoItemsSagaAction.ts
@@ -2,4 +2,18 @@ import { TodoSagaActions } from './index';
 
 export type GetDataTodoItemsSagaAction = {
   type: TodoSagaActions.GET_DATA_ITEMS_SAGA;
+  payload: GetDataTodoItemsSagaActionPayload;
+};
+
+export type GetDataTodoItemsSagaActionPayload = {
+  setLoading: (value: boolean) => void;
+};
+
+export const getDataTodoItemsSagaAction = (
+  payload: GetDataTodoItemsSagaActionPayload,
+) => {
+  return {
+    type: TodoSagaActions.GET_DATA_ITEMS_SAGA,
+    payload,
+  };
 };
