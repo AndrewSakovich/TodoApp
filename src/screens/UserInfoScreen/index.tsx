61b383f0ec98f6b3289ac8db5475523baf7b918a
Andrei Sakovich <andreialekseevich51@gmail.com>
2022-03-21 15:21:07 +0300

    create notification after click

--- src/screens/UserInfoScreen/index.tsx
+++ src/screens/UserInfoScreen/index.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ReduxStoreType, RootStateType } from '../../redux/store';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { TodoReducerState } from '../../redux/reducers/todoReducer';
 import { SignOutButton } from '../../components/SignOutButton';
 import { style } from './style';
@@ -10,8 +10,38 @@ import { signOutSagaAction } from '../../redux/actions/authSagaActions/signOutSa
 import { userSelector } from '../../redux/selectors/userSelector';
 import { todoItemsSelector } from '../../redux/selectors/todoItemsSelector';
 import { AuthReducerState } from '../../redux/reducers/authReducer';
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
 
 export const UserInfoScreen: FC = () => {
+  const getDeviceToken = async () => {
+    const token = await messaging().getToken();
+    console.log('token ', token);
+  };
+
+  const notif = async () => {
+    await PushNotification.localNotificationSchedule({
+      channelId: getDeviceToken,
+      message: 'hello',
+      title: 'hello title',
+      date: new Date(Date.now() + 5 * 1000), // in 60 secs
+      allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
+
+      /* Android Only Properties */
+      repeatTime: 1,
+    });
+  };
+
+  const getPush = async message => {
+    console.log(message);
+  };
+
+  useEffect(() => {
+    const unsub = messaging().onMessage(getPush);
+    getDeviceToken();
+    return unsub;
+  }, []);
+
   const dispatch = useDispatch();
 
   const userInfo = useSelector<RootStateType, AuthReducerState['user']>(
@@ -60,6 +90,9 @@ export const UserInfoScreen: FC = () => {
         </View>
       </View>
       <SignOutButton googleSignOut={googleSignOut} />
+      <TouchableOpacity onPress={notif}>
+        <Text>{'clickk'}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
