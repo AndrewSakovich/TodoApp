765b29adcdbf36e337de04050ebc68e83fc8ec36
AndrewSakovich <94710168+AndrewSakovich@users.noreply.github.com>
2022-04-01 12:18:57 +0300

    refactor notification ios

--- src/helpers/createNotificationHelper.ts
+++ src/helpers/createNotificationHelper.ts
@@ -1,55 +1,50 @@
-import PushNotification from "react-native-push-notification";
-import { AuthReducerState } from "../redux/reducers/authReducer";
-import { TodoItemType } from "../models";
-import { Platform } from "react-native";
-import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from 'react-native-push-notification';
+import { AuthReducerState } from '../redux/reducers/authReducer';
+import { TodoItemType } from '../models';
+import { Platform } from 'react-native';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 export type CreateNotificationHelperDataType = {
-  channelId: AuthReducerState["deviceToken"];
+  channelId: AuthReducerState['deviceToken'];
   newItem: TodoItemType;
 };
 
 export const createNotificationHelper = (
-  notificationData: CreateNotificationHelperDataType
+  notificationData: CreateNotificationHelperDataType,
 ) => {
   const { channelId, newItem } = notificationData;
 
-  if (Platform.OS === "ios") {
-
+  if (Platform.OS === 'ios') {
     PushNotification.configure({
-      // (optional) Called when Token is generated (iOS and Android)
-      onRegister: function(token) {
-        // console.log('TOKEN:', token);
+      onRegister: ({ token }) => {
+        return token;
       },
-      onNotification: function(notification) {
-        console.log("NOTIFICATION:", notification);
+      onNotification: notification => {
         notification.finish(PushNotificationIOS.FetchResult.NoData);
       },
       popInitialNotification: true,
       requestPermissions: true,
-      // IOS ONLY (optional): default: all - Permissions to register.
       permissions: {
         alert: true,
         badge: false,
-        sound: false
-      }
+        sound: false,
+      },
     });
 
     PushNotification.createChannel(
       {
-        channelId: channelId, // (required)
-        channelName: "Task reminder notifications", // (required)
-        channelDescription: "Reminder for any tasks"
+        channelId: channelId,
+        channelName: 'Task reminder notifications',
+        channelDescription: 'Reminder for any tasks',
       },
-      () => {
-      }
+      () => {},
     );
 
     return PushNotificationIOS.addNotificationRequest({
       id: newItem.notificationId,
       body: `your task: ${newItem.text}, not implemented`,
       fireDate: date,
-      repeats: false
+      repeats: false,
     });
   }
 
@@ -59,6 +54,6 @@ export const createNotificationHelper = (
     message: `your task: ${newItem.text}, not implemented`,
     date: new Date(Date.now() + 5 * 1000),
     allowWhileIdle: false,
-    repeatTime: 1
+    repeatTime: 1,
   });
 };
