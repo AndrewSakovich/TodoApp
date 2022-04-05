d7fcbdc5ce676b46ba2f4f296e435e1629112155
AndrewSakovich <94710168+AndrewSakovich@users.noreply.github.com>
2022-04-05 12:47:50 +0300

    add setLoading in AuthSagaActionProps

--- src/redux/actions/authSagaActions/facebookSignInSagaAction.ts
+++ src/redux/actions/authSagaActions/facebookSignInSagaAction.ts
@@ -1,11 +1,19 @@
 import { AuthSagaActions } from './index';
+import React from 'react';
 
 export type FacebookSignInSagaAction = {
   type: AuthSagaActions.FACEBOOK_SIGN_IN_SAGA;
+  payload: FacebookSignInSagaActionPayload;
+};
+export type FacebookSignInSagaActionPayload = {
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export const facebookSignInSagaAction = () => {
+export const facebookSignInSagaAction = (
+  payload: FacebookSignInSagaActionPayload,
+) => {
   return {
     type: AuthSagaActions.FACEBOOK_SIGN_IN_SAGA,
+    payload,
   };
 };
--- src/redux/actions/authSagaActions/googleSignInSagaAction.ts
+++ src/redux/actions/authSagaActions/googleSignInSagaAction.ts
@@ -1,11 +1,19 @@
 import { AuthSagaActions } from './index';
+import React from 'react';
 
 export type GoogleSignInSagaAction = {
   type: AuthSagaActions.GOOGLE_SIGN_IN_SAGA;
+  payload: GoogleSignInSagaActionPayload;
+};
+export type GoogleSignInSagaActionPayload = {
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export const googleSignInSagaAction = () => {
+export const googleSignInSagaAction = (
+  payload: GoogleSignInSagaActionPayload,
+) => {
   return {
     type: AuthSagaActions.GOOGLE_SIGN_IN_SAGA,
+    payload,
   };
 };
