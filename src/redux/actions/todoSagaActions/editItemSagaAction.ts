7f6245db1e842393d80b9e0673556fcbb6cb68c1
AndrewSakovich <94710168+AndrewSakovich@users.noreply.github.com>
2022-04-11 12:17:32 +0300

    create editItemSagaAction.ts

---
+++ src/redux/actions/todoSagaActions/editItemSagaAction.ts
@@ -0,0 +1,19 @@
+import { TodoSagaActions } from './index';
+import { TodoItemType } from '../../../models';
+
+export type EditItemSagaAction = {
+  type: TodoSagaActions.EDIT_ITEM_SAGA;
+  payload: EditItemSagaPayload;
+};
+
+export type EditItemSagaPayload = {
+  id: TodoItemType['id'];
+  text: TodoItemType['text'];
+};
+
+export const editItemSagaActoin = (payload: EditItemSagaPayload) => {
+  return {
+    type: TodoSagaActions.EDIT_ITEM_SAGA,
+    payload,
+  };
+};
