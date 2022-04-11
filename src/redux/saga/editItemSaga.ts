8be3930a22e7999d764cafa2714ceb1530712131
AndrewSakovich <94710168+AndrewSakovich@users.noreply.github.com>
2022-04-11 12:32:10 +0300

    edit item in firebase

--- src/redux/saga/editItemSaga.ts
+++ src/redux/saga/editItemSaga.ts
@@ -1,5 +1,25 @@
 import { EditItemSagaAction } from '../actions/todoSagaActions/editItemSagaAction';
+import { createReferenceHelper } from '../../helpers/createReferenceHelper';
+import { put, select } from 'redux-saga/effects';
+import { createAlertMessageHelper } from '../../helpers/createAlertMessageHelper';
+import { SuccessSignInPayload } from '../actions/authActions/successSignInAction';
+import { userTokenSelector } from '../selectors/userTokenSelector';
 
 export function* editItemSaga(action: EditItemSagaAction) {
   const { id, text } = action.payload;
+  console.log('text', text);
+  const userToken: SuccessSignInPayload['userToken'] = yield select(
+    userTokenSelector,
+  );
+  try {
+    yield createReferenceHelper
+      .ref(`Users/${userToken}/Todo/${id}/text`)
+      .set(text);
+  } catch (error: any) {
+    createAlertMessageHelper({
+      message: `${error.message}`,
+      title: 'Login error',
+      cancelButtonTitle: 'Cancel',
+    });
+  }
 }
