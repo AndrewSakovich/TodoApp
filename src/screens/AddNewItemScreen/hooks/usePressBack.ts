f6584153afe3f1b367b22722229e4c756ef824ce
AndrewSakovich <94710168+AndrewSakovich@users.noreply.github.com>
2022-04-15 12:13:46 +0300

    create usePressBack.ts

---
+++ src/screens/AddNewItemScreen/hooks/usePressBack.ts
@@ -0,0 +1,26 @@
+import { useCallback } from 'react';
+import { createAlertMessageHelper } from '../../../helpers/createAlertMessageHelper';
+import { useNavigation } from '@react-navigation/native';
+import { AddNewItemScreenNavigationProps } from '../type';
+
+export const usePressBack = (hasUnsavedChanges: boolean) => {
+  const navigation = useNavigation<AddNewItemScreenNavigationProps>();
+  const back = () => {
+    return navigation.goBack();
+  };
+  const onPressBack = useCallback(() => {
+    if (hasUnsavedChanges) {
+      return createAlertMessageHelper({
+        onPress: back,
+        title: 'Discard changes?',
+        message:
+          'You have unsaved changes. Are you sure to discard them and leave the screen?',
+        confirmButtonTitle: 'Discard',
+        cancelButtonTitle: "Don't leave",
+      });
+    }
+
+    return back();
+  }, [hasUnsavedChanges]);
+  return onPressBack;
+};
