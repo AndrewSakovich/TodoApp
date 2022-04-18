034cf0857f0eaf092fa81d38529259178ab1bb4f
AndrewSakovich <94710168+AndrewSakovich@users.noreply.github.com>
2022-04-18 16:16:54 +0300

    create type for useTextInput

--- src/hooks/useTextInput.ts
+++ src/hooks/useTextInput.ts
@@ -1,6 +1,13 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
-export const useTextInput = (initialValue: string) => {
+export type UseTextInputReturn = {
+  value: string;
+  onChangeText: Dispatch<SetStateAction<string>>;
+};
+
+export type UseTextInput = (initialValue: string) => UseTextInputReturn;
+
+export const useTextInput: UseTextInput = initialValue => {
   const [value, setText] = useState(initialValue);
   return {
     value,
