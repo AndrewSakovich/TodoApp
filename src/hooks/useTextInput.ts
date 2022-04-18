53450a053cfe0ecc101e3e615779cd13ca173694
AndrewSakovich <94710168+AndrewSakovich@users.noreply.github.com>
2022-04-18 12:47:43 +0300

    refactor useTextInput.ts

--- src/hooks/useTextInput.ts
+++ src/hooks/useTextInput.ts
@@ -2,9 +2,7 @@ import { useState } from 'react';
 
 export const useTextInput = (initialValue: string) => {
   const [value, setText] = useState(initialValue);
-  const hasUnsavedText = value !== initialValue;
   return {
-    hasUnsavedText,
     value,
     onChangeText: setText,
   };
