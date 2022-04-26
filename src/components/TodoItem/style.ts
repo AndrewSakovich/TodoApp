c9561ae0d1558d3104d0104af618d2fb036a2fe1
AndrewSakovich <94710168+AndrewSakovich@users.noreply.github.com>
2022-04-26 13:54:42 +0300

    add shadow for item

--- src/components/TodoItem/style.ts
+++ src/components/TodoItem/style.ts
@@ -18,10 +18,16 @@ export const style = StyleSheet.create<TodoItemStyleType>({
     justifyContent: 'space-between',
     backgroundColor: COLORS.sapphire,
     padding: 15,
-    marginVertical: 5,
+    marginVertical: 10,
     marginHorizontal: 10,
     borderRadius: 5,
     height: 55,
+    shadowOpacity: 0.1,
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowRadius: 10,
   },
   editing: {
     marginRight: 7,
